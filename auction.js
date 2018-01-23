7bcfd08c29f6a0ff01093fe977cbebe469504f60
David Estlund <31256866+destlund@users.noreply.github.com>
2018-01-22 21:09:46 -0600

    Update auction.js

--- auction.js
+++ auction.js
@@ -1,6 +1,8 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
+// When ready to post an item, just run post(item name, item price)
+var post = require("./post.js");
 
 
 var connection = mysql.createConnection({
